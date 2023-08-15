c71171cd86c27de2b3e56e9e6f02d9a6c7d63cd2
ninaschulte <>
2023-08-15 18:10:34 +0000

    Get data function and valiate for spaces around

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from colorama import just_fix_windows_console
+
+def get_age():
+    """
+    Get user age and validate this data.
+    User must be between 18 and 65.
+    User should not enter any space before and after.
+    User should only enter int and not string data.
+    """
+    while True:
+        age_input = input("Enter your age:")
+        age_input_stripped = age_input.strip() 
+
+        if age_input_stripped != age_input:
+            print("Enter your input without space")
+
+# Get the validated age
+user_age = get_age()
+print("User's age:", user_age)
