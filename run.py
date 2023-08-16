1149339ae2175923625e8fa922e038170004f84a
ninaschulte <>
2023-08-16 06:55:51 +0000

    Add function to get retirement age

--- run.py
+++ run.py
@@ -25,6 +25,33 @@ def get_age():
         except ValueError:
             print("Invalid input. Please enter an integer.")
 
+def get_age_retirement():
+    """
+    Get user age and validate this data.
+    User must be between 18 and 65.
+    User should not enter any space before and after.
+    User should only enter int and not string data.
+    """
+    while True:
+        age_input = input("Enter your retirement age:")
+        age_input_stripped = age_input.strip() 
+
+        if age_input_stripped != age_input:
+            print("Enter your input without space")
+            continue
+
+        try:
+            age_retirement = int(age_input_stripped)
+            if 18 <= age_retirement:
+                print("Your age is valid.")
+                return age_retirement
+            else:
+                print("Your age must be between 18 and 65.")
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+
 # Get the validated age
 user_age = get_age()
 print("User's age:", user_age)
+user_retirement_age = get_age_retirement()
+print("User's retirement age:", user_retirement_age)
