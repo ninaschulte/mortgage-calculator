e06aabd69f1c3df6030a9044fef7ce3c08d6d3c5
ninaschulte <>
2023-08-18 07:17:28 +0000

    Add \n to every print statement

--- run.py
+++ run.py
@@ -10,18 +10,18 @@ def validate_age(prompt):
         age_input_stripped = age_input.strip()
 
         if age_input_stripped != age_input:
-            print("Enter your input without space")
+            print("Enter your input without space\n")
             continue
 
         try:
             age = int(age_input_stripped)
             if age >= 18:
-                print("Your age is valid.")
+                print("Your age is valid.\n")
                 return age
             else:
-                print("Your age must be above 18.")
+                print("Your age must be above 18.\n")
         except ValueError:
-            print("Invalid input. Please enter an integer.")
+            print("Invalid input. Please enter an integer.\n")
 
 
 def get_age():
@@ -29,10 +29,10 @@ def get_age():
     Function to get user input on age and retirement.
     Above function then validates this data.
     """
-    age_data = validate_age("Enter your age:")
-    age_retirement = validate_age("Enter your retirement age:")
+    age_data = validate_age("Enter your age:\n")
+    age_retirement = validate_age("Enter your retirement age:\n")
 
-    print("All your data is valid.")
+    print("All your data is valid.\n")
     return age_data, age_retirement
 
 
@@ -45,14 +45,14 @@ def validate_user_input(message):
         user_stripped = user_input.strip()
 
         if user_stripped != user_input:
-            print("Enter your input without space")
+            print("Enter your input without space\n")
             continue
 
         try:
             data = int(user_stripped)
             return data
         except ValueError:
-            print("Invalid input. Please enter an integer.")
+            print("Invalid input. Please enter an integer.\n")
 
 
 def get_money_info():
@@ -60,11 +60,11 @@ def get_money_info():
     Function to get the user input.
     After that data is validated with the validate_user_input function.
     """
-    salary = validate_user_input("Enter your nett monthly salary:")
-    expense = validate_user_input("Enter your sum of food/leisure expenses:")
-    other_expense = validate_user_input("Enter your sum of other expenses (other loans etc.):")
+    salary = validate_user_input("Enter your nett monthly salary:\n")
+    expense = validate_user_input("Enter your sum of food/leisure expenses:\n")
+    other_expense = validate_user_input("Enter your sum of other expenses (other loans etc.):\n")
 
-    print("All your data is valid.")
+    print("All your data is valid.\n")
     return salary, expense, other_expense
 
 
@@ -95,23 +95,23 @@ def main():
     """
     This is the main function that gets all the validated and calculated data in the end.
     """
-    user_age = validate_age("Enter your age:")
-    print("User's age:", user_age)
+    user_age = validate_age("Enter your age:\n")
+    print("User's age:\n", user_age)
 
-    user_retirement_age = validate_age("Enter your retirement age:")
-    print("User's retirement age:", user_retirement_age)
+    user_retirement_age = validate_age("Enter your retirement age:\n")
+    print("User's retirement age:\n", user_retirement_age)
 
     user_salary, user_expense, user_other_expense = get_money_info()
-    print("User's salary:", user_salary)
-    print("User's expenses:", user_expense)
-    print("User's other expenses:", user_other_expense)
+    print("User's salary:\n", user_salary)
+    print("User's expenses:\n", user_expense)
+    print("User's other expenses:\n", user_other_expense)
 
     calculator = Calculator(user_age, user_retirement_age, user_salary, user_expense, user_other_expense)
     user_monthly_investment = calculator.calculate_month()
-    print("Per month you can spend:", user_monthly_investment)
+    print("Per month you can spend:\n", user_monthly_investment)
     user_total_investment = calculator.calculate_total()
-    print("You can afford to buy property in:", user_total_investment)
+    print("You can afford to buy property in:\n", user_total_investment)
 
 
-print("Welcome to the Mortgage calculator.\nYou need to type your input without space.\nType only integer.")
+print("Welcome to the Mortgage calculator.\nYou need to type your input without space.\nType only integer.\n")
 main()
